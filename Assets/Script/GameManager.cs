dd43753bf38772e0d80b688c691a63c4efcdb5d2
1811140054k <1811140054k@ed.fuk.kindai.ac.jp>
2021-03-08 17:58:20 +0900

    Photon ルーム入室、退室管理　

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,6 +10,22 @@ namespace Com.MyCompany.MyGame
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
+        #region Private Methods
+
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+
+        #endregion
+
+
 
         #region  Photon Callbacks
 
@@ -19,6 +35,25 @@ namespace Com.MyCompany.MyGame
         public override void OnLeftRoom()
         {
             SceneManager.LoadScene(0);
+        }
+
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName);
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}",PhotonNetwork.IsMasterClient);
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+
+            LoadArena();
         }
 
         #endregion
